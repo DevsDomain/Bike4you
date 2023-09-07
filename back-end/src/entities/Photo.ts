93386fbae04eb8b103b6dd35ba4bb7075c787af8
Jummanso <julianamaciel97@gmail.com>
2023-09-06 21:41:07 -0300

    inserção pasta photo

---
+++ back-end/src/entities/Photo.ts
@@ -0,0 +1,16 @@
+
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Bike } from "./Bike";
+
+@Entity({name:"photos"})
+export class Photo{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({nullable:false, length:100})
+    filename: string;
+
+    @ManyToOne(() => Bike, (bike) => bike.photos)
+    @JoinColumn({name:"idbike"})
+    bike: Bike;
+}
