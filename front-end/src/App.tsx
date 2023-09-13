ba188b6088cf9fb5645c7834bbfb8c4830e3bf5f
Michael morais <53665466+itsmorais@users.noreply.github.com>
2023-09-12 22:09:27 -0300

    Update README.md

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,43 +1,8 @@
-import { useState } from "react";
-import axios from "axios";
+import User from "./Components/User";
 
 
 function App() {
-  const [alias, setAlias] = useState("");
-  const [mail, setMail] = useState("nome@meuEmail.com")
-  const [phone, setPhone] = useState("996791297")
 
-  async function CriarUsuario(alias: string, mail: string, phone: string): Promise<String> {
-    await axios({
-      method: 'post',
-      url: 'http://localhost:3026/usuario',
-      headers: {},
-      data: {
-        alias,
-        mail,
-        phone
-      }
-    }).then((response) => console.log(response.data))
-    return "Done"
-  }
-
-
-  return (
-
-    <div className="App">
-      <h1>Cadastrar usuários!</h1>
-      <h2>Olá {alias} </h2>
-      <p>Seu email: {mail}</p>
-      <p>Seu telefone: {phone}</p>
-      Seu nome:<input onChange={e => setAlias(e.target.value)} type="text" name="nome" id="meuID" />
-      Seu Email:<input onChange={e => setMail(e.target.value)} type="email" name="mail" id="meuMail" />
-      Seu Telefone:<input onChange={e => setPhone(e.target.value)} type="tel" name="phone" id="meuPhone" />
-      <button onClick={(e: any) => CriarUsuario(alias, mail, phone)}
-      >Enviar para o servidor</button>
-
-
-    </div>
-  );
+  return User();
 }
-
 export default App;
