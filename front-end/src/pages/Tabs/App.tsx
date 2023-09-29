a96a802577c5201671f99930601050ca79f6e950
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-09-29 18:39:52 -0300

    testando tabs

---
+++ front-end/src/pages/Tabs/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Tabs from "./Tabs"
+import Tab from "./Tab"
+
+function App() {
+  return (
+    <Tabs>
+      <Tab title="Lemon">Lemon is yellow</Tab>
+      <Tab title="Strawberry">Strawberry is red</Tab>
+      <Tab title="Pear">Pear is green</Tab>
+    </Tabs>
+  )
+}
