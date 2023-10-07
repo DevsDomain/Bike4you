449387a24579e02b6b4fd0ab5a25664f5393a655
Michael Morais <michael_morais@Outlook.com.br>
2023-10-07 20:40:33 -0300

    Removido codigo de login - movido para o navbar

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -1,20 +1,6 @@
 import styles from '../Home/Home.module.css'
 import Bike4you from '../../assets/bike4you_home.jpg'
-import { useEffect, useState } from "react";
-
 function Home() {
-  const [idUsuario, setidUsuario] = useState("");
-
-
-  useEffect(() => {
-    setidUsuario(JSON.parse(localStorage.getItem("idUsuario") || ""));
-
-    if (idUsuario) {
-      setidUsuario(idUsuario);
-    }
-  }, [idUsuario]);
-
-
   return (
     <div className={styles.home_container}>
       <h1>
