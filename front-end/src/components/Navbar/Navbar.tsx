f7504bd42db4d691dbf08c1834462653afb8af8a
Michael Morais <michael_morais@Outlook.com.br>
2023-10-07 20:38:10 -0300

    Criado icone de logout e inserido nome do usuario quando logado

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -2,10 +2,26 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faBicycle, faCog, faUser } from "@fortawesome/free-solid-svg-icons";
 import styles from './Navbar.module.css'
-
+import { useState, useEffect } from "react";
+import { useAuth } from "../../hooks/auth";
 function Navbar() {
+
+  const [userName, setUser] = useState<string | null>(null)
+
+  const { handleLogOut ,user} = useAuth()
+
+  useEffect(() => {
+    localStorage['userName'] && setUser(localStorage['userName'])
+  }, [user]);
+
+
+  function logout() {
+    handleLogOut()
+    document.location.reload()
+
+  }
   return (
-    <div className={styles.navbar}>  
+    <div className={styles.navbar}>
       <ul className={styles.list}>
         <li className={styles.item}>
           <Link to="/">
@@ -22,11 +38,27 @@ function Navbar() {
             <FontAwesomeIcon icon={faCog} /> Gerenciamento
           </Link>
         </li>
-        <li className={styles.item}>
-          <Link to="/login">
-            <FontAwesomeIcon icon={faUser} /> Login
-          </Link>
-        </li>
+
+
+        {
+          (userName) ? <>
+            <li className={styles.item}>
+              <Link to="/" onClick={logout}><FontAwesomeIcon icon={faUser} />  Logout</Link>
+            </li>
+
+
+            <li className={styles.item}>
+              Olá,<strong>{userName}</strong>
+            </li>
+
+          </>
+
+
+            : <Link to="/login"><FontAwesomeIcon icon={faUser} />  Login</Link>
+
+
+        }
+        
       </ul>
     </div>
   );
