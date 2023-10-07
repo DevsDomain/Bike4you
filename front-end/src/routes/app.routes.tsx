fd7b7ab21e21babe9c9fa56c419bc694432ab355
Claudia Nunes <104475381+Claudia-Nunes@users.noreply.github.com>
2023-10-06 21:52:11 -0300

    Adicionado componente Navbar e pagina Home

--- front-end/src/routes/app.routes.tsx
+++ front-end/src/routes/app.routes.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Cadastro from "../pages/Cadastro";
+import Home from "../pages/Home";
 
 export function AppRoutes() {
     return (
         <Routes>
-
-            <Route path="/" element={<Cadastro />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
 
 
         </Routes>
