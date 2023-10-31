df2d57973f7bdb260c2de41b59da92bfaa6353c9
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2023-10-30 21:56:47 -0300

    Inicializado Componete Rating

--- front-end/src/components/rating/ratings.tsx
+++ front-end/src/components/rating/ratings.tsx
@@ -4,30 +4,17 @@ import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 
 function BasicRating() {
-  const [value, setValue] = React.useState<number | null>(2);
+  const [value, setValue] = React.useState<number | null>(0);
 
   return (
     <Box
       sx={{
-        "& > legend": { mt: 2 },
+        "& > legend": { mt: 0 },
       }}
     >
-      <Typography component="legend">Controlled</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      <Typography component="legend">Read only</Typography>
+    
+      <Typography component="legend"></Typography>
       <Rating name="read-only" value={value} readOnly />
-
-      <Typography component="legend">Disabled</Typography>
-      <Rating name="disabled" value={value} disabled />
-
-      <Typography component="legend">No rating given</Typography>
-      <Rating name="no-value" value={null} />
 
     </Box>
   );
