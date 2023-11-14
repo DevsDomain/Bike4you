1fbf9b5e037c97cf000cf49a6922b815bed61c22
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2023-11-13 21:19:33 -0300

    Nodal Dialogo

---
+++ front-end/src/components/modalDialog/modal.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import avatar from '../../assets/avatar.png'
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function ContactDialogs() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Ver contato
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+
+        
+
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          NOME DO LOCADOR
+        </DialogTitle>
+        
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            1o dado - 
+          </Typography>
+          <Typography gutterBottom>
+            2o dado - 
+          </Typography>
+          <Typography gutterBottom>
+            3o dado - 
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Ok
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </React.Fragment>
+  );
+}
